0961d99f74088dcade779ac246128aec725e016a
raifpy <50047969+raifpy@users.noreply.github.com>
2020-12-22 03:09:30 +0300

    map

--- upFormFile/upFormFile.go
+++ upFormFile/upFormFile.go
@@ -31,3 +31,28 @@ func Upload(method, url string, file io.Reader, fileid, filename string) (*http.
 	return client.Do(request)
 
 }
+
+//MultiUpload abc
+func MultiUpload(method, url string, files map[string]io.Reader) (*http.Response, error) {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	for key, value := range files {
+		part, err := writer.CreateFormFile(key, key)
+
+		if err != nil {
+			return nil, err
+		}
+		io.Copy(part, value)
+	}
+	writer.Close()
+
+	request, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+	client := http.Client{}
+	return client.Do(request)
+
+}
+
